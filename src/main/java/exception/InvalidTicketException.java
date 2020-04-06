59dafd7d96b0c202b680badb5479459afb9320e0
RicoTech666 <zhoujiachenbe@gmail.com>
2020-04-06 22:53:06 +0800

    [Jiachen] feat: add exceptions

--- src/main/java/exception/InvalidTicketException.java
+++ src/main/java/exception/InvalidTicketException.java
@@ -1,5 +1,11 @@
 package exception;
 
 public class InvalidTicketException extends RuntimeException {
+    public InvalidTicketException(String message) {
+        super(message);
+    }
 
+    public InvalidTicketException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/exception/ParkingLotFullException.java
+++ src/main/java/exception/ParkingLotFullException.java
@@ -1,5 +1,11 @@
 package exception;
 
 public class ParkingLotFullException extends RuntimeException {
+    public ParkingLotFullException(String message) {
+        super(message);
+    }
 
+    public ParkingLotFullException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
